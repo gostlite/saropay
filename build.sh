7dd32e0c7ade9012437d04e0db005dd4e42411ba
John Adeleke <foodhubnig@gmail.com>
2025-11-20 00:09:39 +0100

    work

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 
+python manage.py createsuperuser --no-input || true
 python manage.py makemigrations
 python manage.py migrate
 
 
-# python manage.py createsuperuser --no-input || true
