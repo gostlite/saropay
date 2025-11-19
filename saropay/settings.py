7af2a6f35f367e9c14ddb14f8f2206f775baa2a9
John Adeleke <foodhubnig@gmail.com>
2025-11-19 12:49:44 +0100

    media

--- saropay/settings.py
+++ saropay/settings.py
@@ -217,12 +217,18 @@ if IS_HEROKU_APP:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 else:
     # Local storage for development
+  # Static files (CSS, JavaScript, Images)
     STATIC_URL = '/static/'
-    STATIC_ROOT = BASE_DIR / "staticfiles"
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # For media files (uploaded images)
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+    # Additional locations of static files
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ]
     
     # Use simpler storage that doesn't process CSS
     STORAGES = {
