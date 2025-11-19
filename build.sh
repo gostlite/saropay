3c1476c373384075190dcd3b16cead4d59fcb295
John Adeleke <foodhubnig@gmail.com>
2025-11-19 18:01:18 +0100

    work

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python3.11 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
-python3.11 manage.py migrate
+python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python3.11 sorapay/manage.py createsuperuser --no-input
-fi
+
+python sorapay/manage.py createsuperuser --no-input || true
